fdcda0b5a324c6bdb7ba5c3a2b76904f964f34f2
helder <helder.ferreira@pingpost.pt>
2017-11-27 13:55:17 +0000

    -> Complete property binding lesson

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-servers',
     // selector: '[app-servers]',
     // selector: '.app-servers',
-    template: `<app-server></app-server><p>The servers component with inline template</p>`,
+    // template: `<app-server></app-server><p>The servers component with inline template</p>`,
+    templateUrl: './servers.component.html',
     styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer: boolean = false;
 
-    constructor() { }
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 5000);
+    }
 
     ngOnInit() {
     }
