bef96007bdc894cb83e7bf177bef4577cff8a105
helder <helder.ferreira@pingpost.pt>
2017-11-29 14:32:17 +0000

    -> Complete the two way databinding lesson

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import {Component, OnInit} from '@angular/core';
 export class ServersComponent implements OnInit {
     allowNewServer: boolean = false;
     serverCreationStatus: string = 'Server was not created yet!';
-    serverName = '';
+    serverName = 'Test name server';
 
     constructor() {
         setTimeout(() => {
