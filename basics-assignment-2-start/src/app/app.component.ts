cfc946670feb3dd8e6c3f0f6dbbf13a61919274d
Anlh <heldern19@gmail.com>
2017-11-29 23:57:30 +0000

    -> Finish assignment 2

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    userName: string = '';
+
+    onResetUserName(): void {
+        this.userName = '';
+    }
+
+    isUserNameEmpty(): boolean {
+        return this.userName.length === 0;
+    }
 }
