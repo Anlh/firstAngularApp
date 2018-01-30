7827a643af4427025a4636e4aa4ad5bbcdc05fb2
Anlh <heldern19@gmail.com>
2018-01-30 23:03:35 +0000

    -> Done lesson 78

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -6,5 +6,9 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    loadedFeature = 'recipes';
 
+    onMenuLinkClicked(linkMenuClicked: string) {
+        this.loadedFeature = linkMenuClicked;
+    }
 }
--- AngularProject/src/app/header/header.component.ts
+++ AngularProject/src/app/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    @Output() menuLinkClicked = new EventEmitter<string>();
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    onClickNavLinks(navLinkClicked: string) {
+        this.menuLinkClicked.emit(navLinkClicked);
+    }
 
 }
